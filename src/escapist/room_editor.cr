57b15e726de0afa43962292763a80b4d5f7ccdde
Matt Swieboda <mswieboda@users.noreply.github.com>
2024-01-09 20:47:49 -0600

    editor improvements - shows selected type before placing - adds switch sound - adds X icon for removing tile

--- src/escapist/room_editor.cr
+++ src/escapist/room_editor.cr
@@ -6,7 +6,9 @@ module Escapist
     getter view : View
     getter cursor
     property room : Room
+    getter? has_tile
     getter place_sound
+    getter switch_sound
 
     Padding = 56
 
@@ -16,12 +18,26 @@ module Escapist
     PlaceSoundPitchDecrease = 0.33
     PlaceSoundPitchVariation = 0.13
 
+    SwitchSound = SF::SoundBuffer.from_file("./assets/cursor_switch.wav")
+    SwitchSoundVolume = 33
+    SwitchSoundPitchVariation = 0.13
+
+    XFontSize = 48
+    XTextColor = SF::Color.new(153, 0, 0, 125)
+
     def initialize(view, room)
       @view = view
       @room = room
       @cursor = Cursor.new(col: 0, row: 0)
-      @place_sound = SF::Sound.new(PlaceSound)
       @place_type_index = 0
+      @has_tile = false
+      @place_sound = SF::Sound.new(PlaceSound)
+      @switch_sound = SF::Sound.new(SwitchSound)
+      @switch_sound.volume = SwitchSoundVolume
+    end
+
+    def place_type_sym
+      PlaceTypes[@place_type_index]
     end
 
     def update(frame_time, keys : Keys)
@@ -59,24 +75,31 @@ module Escapist
       if keys.just_pressed?(Keys::Tab)
         @place_type_index += 1
         @place_type_index = 0 if @place_type_index > PlaceTypes.size - 1
+
+        unless switch_sound.status == SF::SoundSource::Status::Playing
+          switch_sound.pitch = 1 - SwitchSoundPitchVariation / 2 + rand(SwitchSoundPitchVariation)
+          switch_sound.play
+        end
       end
 
-      if room.tile_obj?(cursor.col, cursor.row)
+      @has_tile = room.tile_obj?(cursor.col, cursor.row)
+
+      if has_tile?
         return unless keys.just_pressed?(Keys::Space)
 
         room.remove_tile_obj(cursor.col, cursor.row)
-        play_sound(remove: true)
+        play_place_sound(remove: true)
       else
-        if place_type = PlaceTypes[@place_type_index]
+        if place_type = place_type_sym
           return unless keys.just_pressed?(Keys::Space)
 
           room.add_tile_obj(place_type, cursor.col, cursor.row)
-          play_sound
+          play_place_sound
         end
       end
     end
 
-    def play_sound(remove = false)
+    def play_place_sound(remove = false)
       unless place_sound.status == SF::SoundSource::Status::Playing
         place_sound.pitch = 1 + ((remove ? -1 : 1) * PlaceSoundPitchDecrease) - PlaceSoundPitchVariation / 2 + rand(PlaceSoundPitchVariation)
         place_sound.play
@@ -85,7 +108,30 @@ module Escapist
 
     def draw(window : SF::RenderWindow)
       room.draw(window, nil)
+
+      if has_tile?
+        draw_x(window)
+      else
+        case place_type_sym
+          when :block
+            Block.new(cursor.col, cursor.row).draw(window)
+          when :floor_switch
+            FloorSwitch.new(cursor.col, cursor.row).draw(window)
+        end
+      end
+
       cursor.draw(window)
+    end
+
+    def draw_x(window)
+      text = SF::Text.new("X", Font.default, XFontSize)
+      text.fill_color = XTextColor
+      text.position = {
+        cursor.x + (cursor.size - text.global_bounds.width) / 2,
+        cursor.y + (cursor.size - text.global_bounds.height) / 2
+      }
+
+      window.draw(text)
     end
   end
 end
